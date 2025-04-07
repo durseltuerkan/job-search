26695dea8f000164391782821d80709e2341b49f
durseltuerkan <dursel.tuerkan@dci-student.org>
2025-04-07 10:26:04 +0200

    server.js ready

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(express.json())
+
+app.listen(port, console.log(`Server started on ${port} `))
